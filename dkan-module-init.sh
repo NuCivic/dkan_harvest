eea78584697fb2560bca46b67a0693da0970c2e5
Ahmed Sghaier <ahmed@nucivic.com>
2016-04-21 02:19:50 +0100

    Civic-2251 Composer path fix and PHP memory update to use 256MB for Circle CI

--- dkan-module-init.sh
+++ dkan-module-init.sh
@@ -6,13 +6,12 @@ DKAN_MODULE="dkan_harvest"
 # DKAN branch to use
 DKAN_BRANCH="dev-dkan-ahoy-smarter"
 
-COMPOSER_PATH=".composer/vendor/bin"
+COMPOSER_PATH="$HOME/.config/composer/vendor/bin"
 
-#Fix for probo not setting the composer path.
 if [[ "$PATH" != *"$COMPOSER_PATH"* ]]; then
   echo "> Composer PATH is not set. Adding temporarily.. (you should add to your .bashrc)"
   echo "PATH (prior) = $PATH"
-  export PATH="$PATH:~/$COMPOSER_PATH"
+  export PATH="$PATH:$COMPOSER_PATH"
 fi
 
 wget -q -O - https://raw.githubusercontent.com/NuCivic/dkan/dev-dkan-ahoy-smarter/dkan-init.sh | bash /dev/stdin $DKAN_MODULE $@ --skip-reinstall
