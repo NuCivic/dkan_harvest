e425327594ed0bf874291bf246198b1709027ae8
Mariano Carballal <marianocarballal@gmail.com>
2016-08-04 16:27:07 -0300

    Tests fixes and code fixes

--- test/behat/features/bootstrap/HarvestSourceContext.php
+++ test/behat/features/bootstrap/HarvestSourceContext.php
@@ -1,10 +1,10 @@
 <?php
 
-use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\DKANExtension\Context\RawDKANEntityContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
-use Drupal\DKANExtension\Context\RawDKANEntityContext;
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
 
 /**
  * Defines application features from the specific context.
@@ -28,12 +28,45 @@ class HarvestSourceContext extends RawDKANEntityContext {
   /**
    * Creates harvest sources from a table.
    *
-   * @Given sources:
+   * @Given harvest sources:
    */
   public function addHarvestSources(TableNode $harvestSourcesTable) {
     parent::addMultipleFromTable($harvestSourcesTable);
   }
-  
+
+  /**
+   * Run a harvest on a harvest source.
+   *
+   * @param $machine_name harvest source machine name
+   *
+   * @Given The :machine_name source is harvested
+   *
+   * @throw \Exception
+   */
+  public function theHarvestSourceIsHarvested($machine_name) {
+    $harvest_source = new HarvestSource($machine_name);
+    if (!$harvest_source) {
+      throw new \Exception("Harvest source '$machine_name' not found.");
+    }
+
+    global $user;
+    // Save the original user to set it back later
+    $global_user = $user;
+
+    $user = user_load(1);
+
+    // Harvest Cache
+    dkan_harvest_cache_sources(array($harvest_source));
+    // Harvest Migration of the test data.
+    dkan_harvest_migrate_sources(array($harvest_source));
+
+    // Make sure that we process any index items added after the harvest.
+    $this->searchContext->process();
+
+    // Back global user to the original user. Probably an anonymous.
+    $user = $global_user;
+  }
+
   /**
   * @AfterScenario @harvest_rollback
   */
